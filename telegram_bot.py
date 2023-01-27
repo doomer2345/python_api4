0364ff18dd8d173119ecb4d86a67b1a1a32bc97a
doomer2345 <evpalov.daniil1996@gmail.com>
2023-01-27 09:50:19 +0300

    бот публикует фото в телеграмм канале

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,3 +3,6 @@ import telegram
 
 bot = telegram.Bot(token = "5942208451:AAE7EZv249efjuC6x4kmhOLdtoAA5MZ-ZgM")
 bot.send_message(chat_id="@supephoto", text="Здравстсвуйте")
+file_path = "images/nasa_apod4.jpg"
+with open(file_path, 'rb') as photo:
+    bot.send_photo(chat_id="@supephoto", photo=photo)
