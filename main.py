41507f16b146fcf34c1f467b84a646a3bed9d358
doomer2345 <evpalov.daniil1996@gmail.com>
2023-01-31 10:23:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import requests
-import os
 from urllib.parse import urlparse
 from os.path import splitext
-from dotenv import load_dotenv
 
 
 def dowloand_images(url, filepath, params=None):
@@ -15,9 +13,3 @@ def dowloand_images(url, filepath, params=None):
 def file_extension(url):
     file_path = urlparse(url).path
     return splitext(file_path)[1]
-
-
-if __name__ == '__main__':
-    os.makedirs("images", exist_ok=True)
-    load_dotenv()
-    api_key=os.getenv("API_KEY")
