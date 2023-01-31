45ce31a42cc9f8b62e3bb7ecd91aa47b1f98094a
doomer2345 <evpalov.daniil1996@gmail.com>
2023-01-31 10:19:55 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,12 +9,13 @@ if __name__ == '__main__':
     images = os.listdir("images")
     load_dotenv()
     tg_token = os.getenv("TELEG_TOKEN")
+    chat_id = os.getenv("CHAT_ID")
     bot = telegram.Bot(token=tg_token)
     while True:
         image = random.choice(images)
         images_path = os.path.join("images", image)
         with open(images_path, 'rb') as photo:
-            bot.send_photo(chat_id="@supephoto", photo=photo)
+            bot.send_photo(chat_id=chat_id, photo=photo)
         time.sleep(14400)
 
 
