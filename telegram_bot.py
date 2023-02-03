54c339412c3486c9ffcb678ea26bee03745b0d9e
doomer2345 <evpalov.daniil1996@gmail.com>
2023-02-03 10:44:52 +0300

    вынес конфиг из кода

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,13 +9,14 @@ if __name__ == '__main__':
     images = os.listdir("images")
     load_dotenv()
     tg_token = os.getenv("TELEG_TOKEN")
-    chat_id = os.getenv("CHAT_ID")
+    chat_id = os.getenv("TG_CHAT_ID")
+    post_time = os.getenv("POST_TIME", "14400")
     bot = telegram.Bot(token=tg_token)
     while True:
         image = random.choice(images)
         images_path = os.path.join("images", image)
         with open(images_path, 'rb') as photo:
             bot.send_photo(chat_id=chat_id, photo=photo)
-        time.sleep(14400)
+        time.sleep(int(post_time))
 
 
