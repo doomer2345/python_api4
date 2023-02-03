e829b885de0e2ce4043634da37739969b8c9e287
doomer2345 <evpalov.daniil1996@gmail.com>
2023-02-03 10:44:34 +0300

    вынес конфиг из кода и дал токену специфическое название

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,10 +1,11 @@
 import os
 import requests
 from tools import dowloand_image
+from dotenv import load_dotenv
 
 
-def fetch_spacex_image():
-    spacex_url = "https://api.spacexdata.com/v5/launches/5eb87d42ffd86e000604b384"
+def fetch_spacex_image(spacex_iaunch_id):
+    spacex_url = f"https://api.spacexdata.com/v5/launches/{spacex_iaunch_id}"
     response = requests.get(spacex_url)
     response.raise_for_status()
     links = response.json()["links"]["flickr"]["original"]
@@ -13,5 +14,7 @@ def fetch_spacex_image():
         dowloand_image(link, filepath)
 
 if __name__ == '__main__':
+    load_dotenv()
+    spacex_iaunch_id = os.getenv("SPACEX_LAUNCH_ID", "5eb87d42ffd86e000604b384")
     os.makedirs("images", exist_ok=True)
-    fetch_spacex_image()
+    fetch_spacex_image(spacex_iaunch_id)
